f13824d38e6a8a9abcdaa38b37d3a6fe0f4f2f74
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-03-10 22:11:05 -0300

    Update audio

--- Projetos/PythonExercicios/ex021.py
+++ Projetos/PythonExercicios/ex021.py
@@ -4,7 +4,7 @@ print('Exercício 21')
 # Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.
 
 pygame.mixer.init()
-pygame.mixer.music.load('vtncdiscord.mp3')
+pygame.mixer.music.load('coisasestranhas.mp3')
 pygame.mixer.music.play()
 input()
 pygame.event.wait()
